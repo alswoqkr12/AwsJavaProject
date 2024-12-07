13ab8767b342fcc6725632fd6e2b2901cb7f9fba
MinJaePark <qkr2817@gmail.com>
2024-12-07 21:41:07 +0900

    커밋

--- src/main/java/com/example/AwsChProject/config/WebConfig.java
+++ src/main/java/com/example/AwsChProject/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대해 CORS 허용
                 .allowedOrigins("http://107.20.189.174:3000")// React 앱 URL
-                .allowedOrigins("http://localhost:3000")
+                //.allowedOrigins("http://localhost:3000")
                 .allowedMethods("*") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 인증 정보 허용 (쿠키, Authorization 등)
