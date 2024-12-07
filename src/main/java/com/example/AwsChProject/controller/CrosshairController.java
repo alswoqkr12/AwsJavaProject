86316e3523671442e3ac6548ea6c501e7f4b9a99
MinJaePark <qkr2817@gmail.com>
2024-12-07 19:36:48 +0900

    asdasd

--- src/main/java/com/example/AwsChProject/controller/CrosshairController.java
+++ src/main/java/com/example/AwsChProject/controller/CrosshairController.java
@@ -2,36 +2,34 @@ package com.example.AwsChProject.controller;
 
 import com.example.AwsChProject.model.Crosshair;
 import com.example.AwsChProject.repository.CrosshairRepository;
-import com.example.AwsChProject.service.S3Service;
 
-import lombok.RequiredArgsConstructor;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/crosshairs")
-@RequiredArgsConstructor // final 필드에 대한 생성자를 자동으로 생성
 public class CrosshairController {
 
-    private CrosshairRepository crosshairRepository; // final 필드
-    private S3Service s3Service; // final 필드
+
+    @Autowired
+    private CrosshairRepository crosshairRepository;
+
 
     // Crosshair 업로드 (이미지 파일을 S3에 업로드하고 RDS에 URL 저장)
     @PostMapping
     public Crosshair uploadCrosshair(@RequestParam("name") String name,
                                      @RequestParam("description") String description,
                                      @RequestParam("code") String code,
-                                     @RequestParam("image") MultipartFile image) { // MultipartFile 사용
+                                     @RequestParam("image") String image) {
         try {
-            // S3에 이미지 파일 업로드
-            String imageUrl = s3Service.uploadImage(image); // S3Service를 사용해 이미지 URL을 얻음
 
-            // Crosshair 객체 생성, S3 URL을 DB에 저장
-            Crosshair crosshair = new Crosshair(name, description, code, imageUrl);
+            // Crosshair 객체를 생성하고, S3 URL을 DB에 저장
+            Crosshair crosshair = new Crosshair(name, description, code, image);
             return crosshairRepository.save(crosshair);
         } catch (Exception e) {
             throw new RuntimeException("파일 업로드 실패: " + e.getMessage());
         }
     }
+
+
 }
